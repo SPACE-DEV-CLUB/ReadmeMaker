472611f91c59ba697e6aa786d723e72d1bb4d681
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-05-04 20:40:49 +0900

    feat : 템플릿 레이아웃 구현

---
+++ src/components/main/TemplateCard/Tag.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Tag = () => {
+  return <Box></Box>;
+};
+
+const Box = styled.div`
+  width: 94px;
+  height: 28px;
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: orange;
+  margin: 0 20px 21px 0;
+`;
+
+export default Tag;
