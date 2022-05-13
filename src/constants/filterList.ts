b617e98ef2db465488b4a73c4323341677ae70df
ongddree <ongddree@gmail.com>
2022-05-14 01:23:19 +0900

    chore : filterList 상수 데이터 추가

---
+++ src/constants/filterList.ts
@@ -0,0 +1,26 @@
+export const FILTER_LIST = [
+  {
+    id: 1,
+    value: 'all',
+  },
+  {
+    id: 2,
+    value: 'textarea',
+  },
+  {
+    id: 3,
+    value: 'contact',
+  },
+  {
+    id: 4,
+    value: 'backjoon',
+  },
+  {
+    id: 5,
+    value: 'commit',
+  },
+  {
+    id: 6,
+    value: 'hit',
+  },
+];
