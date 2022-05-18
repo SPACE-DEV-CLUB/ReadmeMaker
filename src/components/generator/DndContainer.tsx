2057146c283741c63477f5de52c0f82720e40646
dana <dana.kim@nicetomeetme.kr>
2022-05-18 20:21:01 +0900

    refactor: strictmode 삭제

--- src/components/generator/DndContainer.tsx
+++ src/components/generator/DndContainer.tsx
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import '@atlaskit/css-reset';
 import EditorComponent from './EditorComponent';
-import { uuid } from 'uuidv4';
 
 const DndContainer = ({ post, setPost }: any) => {
   const handleChange = (result: any) => {
@@ -19,13 +18,9 @@ const DndContainer = ({ post, setPost }: any) => {
     <DragDropContext onDragEnd={handleChange}>
       <Droppable droppableId="cardlists">
         {provided => (
-          <CardListContainer
-            className="cardlists"
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
+          <div className="cardlists" {...provided.droppableProps} ref={provided.innerRef}>
             {post.map((e: any, i: number) => (
-              <Draggable key={`test-${e.id}`} draggableId={`test-${e.id}`} index={i}>
+              <Draggable draggableId={`test-${e.id}`} index={i} key={`test-${e.id}`}>
                 {(provided, snapshot) => {
                   return (
                     <div
@@ -40,7 +35,7 @@ const DndContainer = ({ post, setPost }: any) => {
               </Draggable>
             ))}
             {provided.placeholder}
-          </CardListContainer>
+          </div>
         )}
       </Droppable>
     </DragDropContext>
@@ -48,5 +43,3 @@ const DndContainer = ({ post, setPost }: any) => {
 };
 
 export default DndContainer;
-
-const CardListContainer = styled.div``;
