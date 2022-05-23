9d6a4b922f02bfcb1a66e618c913ec04605d6d98
ongddree <ongddree@gmail.com>
2022-05-23 23:10:45 +0900

    feat: Editor Container 추가 생성 기기능 구현

--- src/components/generator/Generator.tsx
+++ src/components/generator/Generator.tsx
@@ -1,11 +1,28 @@
 import styled from '@emotion/styled';
+import { useRecoilState } from 'recoil';
+import { v4 as uuid } from 'uuid';
+import { postsState } from 'atoms/posts';
 import EditorComponent from './EditorComponent';
 
 const Generator = () => {
+  const [posts, setPosts] = useRecoilState(postsState);
+
+  const addPost = () => {
+    setPosts(oldPosts => [
+      ...oldPosts,
+      {
+        id: `${uuid()}`,
+        content: '',
+      },
+    ]);
+  };
+
   return (
     <Container>
-      <EditorComponent />
-      <EditorComponent />
+      {posts.map(post => (
+        <EditorComponent key={post.id} post={post} />
+      ))}
+      <BtnAdd onClick={addPost}>추가</BtnAdd>
     </Container>
   );
 };
@@ -18,4 +35,10 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
+`;
+
+const BtnAdd = styled.button`
+  color: white;
+  font-weight: 700;
+  font-size: 20px;
 `;
