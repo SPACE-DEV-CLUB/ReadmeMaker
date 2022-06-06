3c74aa2576dfd0427325da752720e707e259ed60
ongddree <ongddree@gmail.com>
2022-06-06 16:09:48 +0900

    style: editor 메뉴바 UI

--- src/components/generator/editor/TextComponentEditor.tsx
+++ src/components/generator/editor/TextComponentEditor.tsx
@@ -1,10 +1,12 @@
 import { Editor } from '@tinymce/tinymce-react';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { componentsState } from 'atoms/components';
 import { TextComponentType } from 'types/textComponentType';
 import { removeComponent, replaceText } from 'utils';
 import ComponentContainer from './ComponentContainer';
+import { useEffect } from 'react';
 
 interface TextComponentEditorProps {
   textComponent: TextComponentType;
@@ -32,28 +34,31 @@ const TextComponentEditor = ({ textComponent, isDragging }: TextComponentEditorP
 
   return (
     <ComponentContainer isDragging={isDragging}>
-      <ComponentEditor
-        value={textComponent.code}
-        apiKey={process.env.EDITOR_API_KEY}
-        init={{
-          bordercolor: 'blue',
-          height: 30,
-          menubar: false,
-          statusbar: false,
-          skin: 'snow',
-          toolbar_sticky: true,
-          toolbar:
-            'bold | italic | underline | alignleft | aligncenter| alignright | alignjustify ',
-          content_style: `
+      <EditorWrap isDragging={isDragging}>
+        <Editor
+          value={textComponent.code}
+          apiKey={process.env.EDITOR_API_KEY}
+          // TODO: isDragging 글자색상 분기 처리
+          init={{
+            bordercolor: 'blue',
+            height: 30,
+            menubar: false,
+            statusbar: false,
+            skin: 'snow',
+            toolbar_sticky: true,
+            toolbar:
+              'bold | italic | underline | alignleft | aligncenter| alignright | alignjustify ',
+            content_style: `
           body {
             color: #dddedf !important;
           }
           p {
             margin: 0 !important;
           }`,
-        }}
-        onEditorChange={editText}
-      />
+          }}
+          onEditorChange={editText}
+        />
+      </EditorWrap>
       <BtnDelete onClick={deleteTextComponent}>삭제</BtnDelete>
     </ComponentContainer>
   );
@@ -61,23 +66,23 @@ const TextComponentEditor = ({ textComponent, isDragging }: TextComponentEditorP
 
 export default TextComponentEditor;
 
-const ComponentEditor = styled(Editor as any)`
+const EditorWrap = styled.article<{ isDragging: boolean }>`
   & .tox-toolbar__primary {
-    background-color: #000 !important;
+    background-color: ${props => (props.isDragging ? '#F5FF80' : '#0e1116')} !important;
   }
 
   & .tox-editor-header {
-    background-color: #000 !important;
+    // TODO: border style none 해제
+    background-color: ${props => (props.isDragging ? '#f5ff80' : '#0e1116')} !important;
   }
 
   & .tox-toolbar__group {
-    background-color: #171b21;
+    background-color: ${props => (props.isDragging ? '#e9f370' : '#1B2027')} !important;
     width: 24px;
     height: 24px;
-    line-height: 24px !important;
     display: flex;
     justify-content: center;
-    align-items: center !important;
+    align-content: center;
     border-radius: 4px;
     margin-right: 2px !important;
   }
@@ -85,6 +90,7 @@ const ComponentEditor = styled(Editor as any)`
   & .tox .tox-tbtn svg {
     fill: #dddedf;
   }
+
   & .tox-tbtn {
     margin: 0 !important;
   }
@@ -98,7 +104,7 @@ const ComponentEditor = styled(Editor as any)`
   }
 
   & .tox-edit-area__iframe {
-    background-color: #171b21 !important;
+    background-color: ${props => (props.isDragging ? '#e9f370' : '#1B2027')} !important;
     margin-top: 10px;
     height: 54px !important;
     border-radius: 20px;
