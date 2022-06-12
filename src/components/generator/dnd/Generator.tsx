7152a10fb9f50019536145f1f11c9fe50e40e282
deli-ght <minjju0660@gmail.com>
2022-06-13 00:02:00 +0900

    style : 제너레이터 UI 수정

--- src/components/generator/dnd/Generator.tsx
+++ src/components/generator/dnd/Generator.tsx
@@ -5,6 +5,7 @@ import styled from '@emotion/styled';
 import { useRecoilState } from 'recoil';
 import { componentsState } from 'atoms/components';
 import DndComponent from './DndComponent';
+import { darkTheme } from 'styles/theme';
 
 const Generator = () => {
   const [components, setComponents] = useRecoilState(componentsState);
@@ -29,23 +30,46 @@ const Generator = () => {
   };
 
   return (
-    <DragDropContext onDragEnd={handleChange}>
-      <Droppable droppableId="cardlists">
-        {provided => (
-          <div className="cardlists" {...provided.droppableProps} ref={provided.innerRef}>
-            {components.map((component: any, i: number) => (
-              <DndComponent key={`test-${component.id}`} component={component} componentIndex={i} />
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-      <BtnAdd onClick={addTextEditor}>추가</BtnAdd>
-    </DragDropContext>
+    <DndContainer>
+      <h2 className="sr-only">컴포넌트 수정하기</h2>
+      <DragDropContext onDragEnd={handleChange}>
+        <Droppable droppableId="cardlists">
+          {provided => (
+            <div className="cardlists" {...provided.droppableProps} ref={provided.innerRef}>
+              {components.map((component: any, i: number) => (
+                <DndComponent
+                  key={`test-${component.id}`}
+                  component={component}
+                  componentIndex={i}
+                />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        <BtnAdd onClick={addTextEditor}>추가</BtnAdd>
+      </DragDropContext>
+    </DndContainer>
   );
 };
 
 export default Generator;
+
+const DndContainer = styled.section`
+  text-align: center;
+  overflow-y: auto;
+  background-color: ${darkTheme.CASUAL_FIELD};
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: ${darkTheme.CASUAL_FIELD};
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${darkTheme.CASUAL_LINE};
+  }
+`;
 
 const BtnAdd = styled.button`
   color: white;
