39986050fc8859f09ade421490b8f2ad9f9e333c
deli-ght <minjju0660@gmail.com>
2022-06-19 00:05:35 +0900

    feat: generator ssr 적용 해제

--- src/pages/generator.tsx
+++ src/pages/generator.tsx
@@ -2,11 +2,11 @@ import styled from '@emotion/styled';
 import dynamic from 'next/dynamic';
 import React from 'react';
 import NavBar from 'components/common/NavBar';
+import Generator from 'components/generator/dnd/Generator';
 import SideBar from 'components/generator/sidebar/SideBar';
-import PreviewContainer from 'components/generator/viewer/PreviewContainer';
 
 const generator = () => {
-  const Generator = dynamic(() => import('components/generator/dnd/Generator'), {
+  const PreviewContainer = dynamic(() => import('components/generator/viewer/PreviewContainer'), {
     ssr: false,
   });
 
