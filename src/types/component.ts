ea9cb6420d9e08ca33798ec480504dde7f35cf5c
kykim00 <jeongsaeki@likelion.org>
2022-07-03 10:48:21 +0900

    feat: 타입 적용

---
+++ src/types/component.ts
@@ -0,0 +1,32 @@
+export interface Component {
+  ComponentTags: ComponentTag[];
+  TemplateId: number;
+  author: string;
+  code: string;
+  createdAt: string;
+  description: string;
+  id: number;
+  image: string;
+  like: number;
+  link: string;
+  related_template: number;
+  title: string;
+  type: string;
+  updatedAt: string;
+  varaiable: string;
+}
+
+export interface ComponentTag {
+  Function_Tag: FunctionTag;
+  createdAt: string;
+  id: number;
+  title: string;
+  updatedAt: string;
+}
+
+export interface FunctionTag {
+  component_id: number;
+  component_tags_id: number;
+  createdAt: string;
+  updatedAt: string;
+}
