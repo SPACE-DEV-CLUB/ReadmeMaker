209708dcb435d8d828b72d6e2043024305d50b14
ongddree <ongddree@gmail.com>
2022-08-07 19:10:21 +0900

    refactor: component value 수정하는 함수 네이밍 변경

--- src/utils/replaceText.ts
+++
@@ -1,4 +0,0 @@
-// TODO: api에서 type 정해지면 any 수정
-export const replaceText = (arr: Array<any>, curIndex: number, newValue: any) => {
-  return [...arr.slice(0, curIndex), newValue, ...arr.slice(curIndex + 1)];
-};
